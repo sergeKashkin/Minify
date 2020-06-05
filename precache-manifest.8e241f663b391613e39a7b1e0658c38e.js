7ce7de1d8e30885a5a2f2b3cd979b1ac033044b1
sergeKash <39168159+sergeKashkin@users.noreply.github.com>
2020-06-05 11:08:29 +0300

    Updates

---
+++ precache-manifest.8e241f663b391613e39a7b1e0658c38e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad1366bb500abff5b61a8035b39bce1",
+    "url": "/Minify/index.html"
+  },
+  {
+    "revision": "fa79a04bcc38a3e4e45c",
+    "url": "/Minify/static/css/main.f190fc1a.chunk.css"
+  },
+  {
+    "revision": "3558eb9e88cef7a94637",
+    "url": "/Minify/static/js/2.667873c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minify/static/js/2.667873c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa79a04bcc38a3e4e45c",
+    "url": "/Minify/static/js/main.143116a3.chunk.js"
+  },
+  {
+    "revision": "0145b996a8cae223352f",
+    "url": "/Minify/static/js/runtime-main.e00a1cf8.js"
+  }
+]);
